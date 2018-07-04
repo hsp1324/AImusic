7ff6b10a47795a86a4a8a8adbb836a09471e26b5
Sunpyo Hong <sunpyohong@Sunpyos-MacBook-Pro-2.local>
2018-07-04 23:42:28 +0900

    temp finished vecterized but confronting dimension error

--- Vecterize.py
+++ Vecterize.py
@@ -4,7 +4,7 @@ from fractions import Fraction
 from collections import Counter
 
 #c0 - b8
-#vector size = 1(not rest) + 12 * 9(number of notes) + 1(duration) + 1(measure at) + 1(measure left)
+#vector size = 1(rest) + 9(number of octave) + 12(number of names) + 16(number of possible durations) + 1(measure at) + 1(measure left)
 
 """ 
 Duration
@@ -14,133 +14,125 @@ Duration
 """
 
 
-# vector_size = 1 + 12*9 + 1 + 1 + 1
-# measure_at_pos = 1 + 12*9
-vector_size = 12*9
-measure_at_pos = 12*9
-measure_left_pos = measure_at_pos + 1
-duration_pos = measure_left_pos + 1
+# vector_size = 1 + 9 + 12 + 1 + 1 + 1
+# measure_at_index = 1 + 12*9
+number_of_octave = 9
+number_of_names = 12
+number_of_possible_duration = 16
+vector_size = 1 + number_of_octave + number_of_names + number_of_possible_duration + 1 + 1
+octave_pos_in_vector = 1
+name_pos_in_vector = octave_pos_in_vector + number_of_octave
+duration_pos_in_vector = name_pos_in_vector + number_of_names
+measure_at_pos_in_vector = duration_pos_in_vector + number_of_possible_duration
+measure_left_pos_in_vector = measure_at_pos_in_vector + 1
 
-def vectorizePart(part):
-	measures = part.getElementsByClass('Measure')
-	vector = np.zeros(len(measures))  # I am not sure Rest does count here
-	
-	for measure_index in range(len(measures)):
-		iter_measure = measures[measure_index]
-		measure_vector = vectorizeMeasure(iter_measure, measure_index, len(measures))
-		vector[measure_index] = measure_vector
-
-	return vector
-
-
-
-def vectorizeMeasure(measure, measure_num, total_num_measure, total_index=0, partVector=np.empty(1)):  # partVector and total_index are for the test for now
-	allNotes = measure.recurse().notes
-	# vector = np.empty([vector_size, len(allNotes)])  # np.empty or np.zeros
-
-	if len(allNotes) == 0:
-		return partVector
-
-	accum_duration = 0
-	for vector_index in range(len(allNotes)):
-		vector = np.empty(vector_size)  # np.empty or np.zeros
-		iter_item = allNotes[vector_index]
-		iter_note = None
-		if type(iter_item) is note.Note:
-			iter_note = iter_item
-		elif type(iter_note) is chord.Chord:
-			iter_note = iter_item[-1]   # get the hightst picth in the chord for now. We will get all notes in chord in the future
-		elif type(iter_note) is note.Rest:
-			vector[0] = 1  # Might vector[vector_index][0] = 1
-			continue
-		else:
-			print("Type is neighther note nor chord")
-
-		iter_name = iter_note.name
-		iter_octave = iter_note.octave
-		iter_duration = iter_note.duration.quarterLength
-		note_num_pos = name_to_number(iter_name)
-
-		if(iter_octave == 0 and note_pos < 0):
-			print("Invalid Note")
-			continue
-
-		if(iter_octave > 9):
-			print("Octave is too high")
-
-		iter_note_index_in_vector = iter_octave * 12 + note_num_pos + 1  # add 1 for 'not rest'
-
-		vector[total_index][iter_note_index_in_vector] = 1
-		vector[total_index][measure_at_pos] = measure_num
-		vector[total_index][measure_left_pos] = total_num_measure
-		vector[total_index][duration_pos] = iter_duration  # Might want to introduce accum_duration
-
-
-		partVector[total_index+vector_index] = vector
-
-
-	return partVector, vector
 
 
 
 
 def vectorize(part):
 	measures = part.getElementsByClass('Measure')
-	total_num_measure = len(measures)
+	total_num_measures = len(measures)
 	allNotes = part.recurse().notes
-	vector = np.zeros([1, len(allNotes), vector_size])  # np.empty or np.zeros
-
-	accum_duration = 0
-	vector_index = 0
-	measure_index = 0
+	vector = np.zeros([len(allNotes), vector_size])  # np.empty or np.zeros
+	total_accum_duration = 0
+	nth_index = -1
+	measure_index = -1
 	for measure in measures:
-		allNotes = measure.recurse().notes
-		if len(allNotes) == 0:
+		measure_notes = measure.recurse().notes
+		measure_accum_duration = 0
+		if len(measure_notes) == 0:
 			measure_index += 1
-			accum_duration+= 4
+			total_accum_duration+= 4
 			continue
 
-		for iter_item in allNotes:
-			# vector[0][vector_index][0] = 1  # Show that it is not rest
+		for iter_item in measure_notes:
+			nth_index += 1
+			iter_vector = vector[:][nth_index]
+			# vector[0][nth_index][0] = 1  # Show that it is not rest
 			iter_note = None
 			if type(iter_item) is note.Note:
 				iter_note = iter_item
 			elif type(iter_item) is chord.Chord:
 				iter_note = iter_item[-1]   # get the hightst picth in the chord for now. We will get all notes in chord in the future
 			elif type(iter_item) is note.Rest:
-				# vector[vector_index] = 1  # Might vector[vector_index][0] = 1
+				iter_vector[0] = 1  # Might vector[nth_index][0] = 1
+				iter_duration = iter_note.duration.quarterLength
+				duration_index = transform_duration_to_number(iter_duration)
+				iter_vector[duration_pos_in_vector + duration_index] = 1
+				iter_vector[measure_at_pos_in_vector] = measure_index
+				iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
+				measure_accum_duration += iter_duration
 				continue
 			else:
-				print("Type is neighther note nor chord")
+				print("Type is neither note nor chord!!!!!!!!!!")
+				iter_vector[0] = 1  # Might vector[nth_index][0] = 1
+				iter_duration = 1.0
+				duration_index = transform_duration_to_number(iter_duration)
+				iter_vector[duration_pos_in_vector + duration_index] = 1
+				iter_vector[measure_at_pos_in_vector] = measure_index
+				iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
+				measure_accum_duration += iter_duration
+				continue
 
 			iter_name = iter_note.name
 			iter_octave = iter_note.octave
 			iter_duration = iter_note.duration.quarterLength
-			note_num_pos = name_to_number(iter_name)
 
-			if(iter_octave == 0 and note_pos < 0):
-				print("Invalid Note")
-				continue
+			iter_name_number = transform_name_to_number(iter_name)
+			iter_octave = get_octave(iter_octave)
+			duration_index = transform_duration_to_number(iter_duration)
+
+			if(iter_name_number < 0):
+				if(iter_octave != 0):
+					iter_octave -= 1
+					iter_name_number = 12 + iter_name_number
+				else:
+					print("Invalid Note. Consider it as a Rest")
+					iter_vector[0] = 1  
+					iter_vector[duration_pos_in_vector + duration_index] = 1
+					iter_vector[measure_at_pos_in_vector] = measure_index
+					iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
+					measure_accum_duration += iter_duration
+					continue
 
 			if(iter_octave > 9):
-				print("Octave is too high")
+				print("Octave is too high. Consider it as a Rest")
+				iter_vector[0] = 1  
+				iter_vector[duration_pos_in_vector + duration_index] = 1
+				iter_vector[measure_at_pos_in_vector] = measure_index
+				iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
+				measure_accum_duration += iter_duration
+				continue
 
-			# iter_note_index_in_vector = iter_octave * 12 + note_num_pos + 1  # add 1 for 'not rest'
-			iter_note_index_in_vector = iter_octave * 12 + note_num_pos
-			vector[0][vector_index][iter_note_index_in_vector] = 1
-			# vector[0][vector_index][measure_at_pos] = measure_index
-			# vector[0][vector_index][measure_left_pos] = total_num_measure - measure_index - 1
-			# vector[0][vector_index][duration_pos] = iter_duration  # Might want to introduce accum_duration
+			iter_vector[octave_pos_in_vector+iter_octave] = 1
+			iter_vector[name_pos_in_vector+iter_name_number] = 1
+			iter_vector[duration_pos_in_vector+duration_index] = 1
+			iter_vector[measure_at_pos_in_vector] = measure_index
+			iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
 
-			vector_index += 1
 
+			measure_accum_duration += iter_duration
+
+		total_accum_duration += measure_accum_duration
 		measure_index += 1
 
 	return vector
 
 
 
-def name_to_number(name):
+
+def get_octave(octave):
+	if(octave == None):
+		return 4
+	else:
+		return octave
+
+
+
+
+
+def transform_name_to_number(name):
 	pos = 0
 	half = 0
 	aug = 0
@@ -166,7 +158,7 @@ def name_to_number(name):
 	elif(note_name == 'B'):
 		pos = 11
 	else:
-		print("name_to_number is not in A to G")
+		print("transform_name_to_number is not in A to G")
 
 	if(half != 0):
 		if(half == '#'):
@@ -184,13 +176,29 @@ def name_to_number(name):
 
 	return pos
 
+
+
+
+def transform_duration_to_number(duration):
+	possible_duration = [0.0625, Fraction(1,10), Fraction(1,12), 0.125, Fraction(1,6), 0.25, Fraction(1,3),
+						0.5, 0.75, 1.0, 1.5, 1.75, 2.0, 3.0, 4.0, 6.0]
+	duration_index = 0
+	try:
+		duration_index = possible_duration.index(duration)
+		return duration_index
+	except:
+		print("There is no such duration '" + str(duration) + "' in possible_duration list so I will return 1.0")
+		return possible_duration.index(1.0)
+
+
+
 # c = {x:allDuration.count(x) for x in allDuration}
 
-# summer = converter.parse("Summer_Joe_Hisaishi.mxl")
-# part1 = summer.getElementsByClass('Part')[0]
-# measures1 = part1.getElementsByClass('Measure')
-# allNotes1 = measures1.recurse().notes
-# vector1 = vectorize(part1)
+summer = converter.parse("Summer_Joe_Hisaishi.mxl")
+part1 = summer.getElementsByClass('Part')[0]
+measures1 = part1.getElementsByClass('Measure')
+allNotes1 = measures1.recurse().notes
+vector1 = vectorize(part1)
 
 # part2 = summer.getElementsByClass('Part')[1]
 # measures2 = part2.getElementsByClass('Measure')
@@ -202,6 +210,7 @@ def name_to_number(name):
 # song_notes = song_part.recurse().notes
 # song_durations = [i.duration.quarterLength for i in song_notes]
 # song_counter = Counter(song_durations)
+
 
 
 

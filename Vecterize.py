cdf1a37ed250272924a5a7afe1b0d66fde8b6eaa
Sunpyo Hong <sunpyo.hong@samsung.com>
2018-11-28 08:10:16 +0900

    sun music

--- Vecterize.py
+++ Vecterize.py
@@ -6,7 +6,7 @@ import copy
 from keras.preprocessing import sequence
 
 #c0 - b8
-#vector size = 1(rest) + 9(number of octave) + 12(number of names) + 16(number of possible durations) + 1(measure at) + 1(measure left)
+#onehot_size: 1(rest) + 9(number of octave) + 12(number of names) + 16(number of possible durations) Not using{+ 1(measure at) + 1(measure left)}
 
 """ 
 Duration
@@ -16,17 +16,19 @@ Duration
 """
 
 
-# vector_size = 1 + 9 + 12 + 1 + 1 + 1
+
+# onehot_size = 1 + 9 + 12 + 1 + 1 + 1
 # measure_at_index = 1 + 12*9
+MAX_NUM_NOTES = 25   # Max number of notes per Measure
+sequence_length = 0
 number_of_octave = 9
 number_of_names = 12
 number_of_possible_duration = 16
-vector_size = 1 + number_of_octave + number_of_names + number_of_possible_duration + 1 + 1
+onehot_size = 1 + number_of_octave + number_of_names + number_of_possible_duration
 octave_pos_in_vector = 1
 name_pos_in_vector = octave_pos_in_vector + number_of_octave
 duration_pos_in_vector = name_pos_in_vector + number_of_names
-measure_at_pos_in_vector = duration_pos_in_vector + number_of_possible_duration
-measure_left_pos_in_vector = measure_at_pos_in_vector + 1
+
 
 possible_duration = [0.0625, Fraction(1,10), Fraction(1,12), 0.125, Fraction(1,6), 0.25, Fraction(1,3),
 						0.5, 0.75, 1.0, 1.5, 1.75, 2.0, 3.0, 4.0, 6.0]
@@ -34,44 +36,61 @@ possible_duration = [0.0625, Fraction(1,10), Fraction(1,12), 0.125, Fraction(1,6
 name_dic = {0:'C', 1:'C#', 2: 'D', 3: 'D#', 4: 'E', 5: 'F', 6: 'F#', 7: 'G',
 				8: 'G#', 9: 'A', 10: 'A#', 11: 'B'}
 
+sharps_to_diatonic_dic = {-1: -5, -2: 2, -3: -3, -4: 4, -5: -1, -6: -6,
+													0: 0, 1: 5, 2: -2, 3: 3, 4: -4, 5: 1}
+
 duration_dic = {}
 for i in range(len(possible_duration)):
 	duration_dic[i] = possible_duration[i]
 
-def vectorize(part):
+
+
+def vectorize(part, sample_measure_size=1, scale_int=0):   # current scale in int,  C major = 0, D major = 2
+
+	sequence_length = sample_measure_size * MAX_NUM_NOTES
+
 	measures = part.getElementsByClass('Measure')
 	total_num_measures = len(measures)
-	num_all_notes = count_notes(part)
-	vector = np.zeros([num_all_notes, vector_size])  # np.empty or np.zeros
-	total_accum_duration = 0
-	nth_index = -1
-	measure_index = 0
-	for measure in measures:
-		measure_index += 1
-		measure_accum_duration = 0
-		for iter_item in measure:
+	# num_all_notes = count_notes(part)
+	sample_size = total_num_measures - sample_measure_size + 1
+
+	steps_to_move = sharps_to_diatonic_dic[scale_int]  # make every note in diatonic scale   scale_int = 0 for no transpose
+
+
+	ret_vector = np.empty(shape=(0, sequence_length, onehot_size))
+	sample_vector = np.zeros([1, sample_measure_size, MAX_NUM_NOTES, onehot_size])
+	sample_index = 0
+
+
+	for measure_index in range(len(measures)):
+		iter_measure = measures[measure_index]
+		measure_vector = np.zeros([MAX_NUM_NOTES, onehot_size])
+
+		note_index = 0
+		for iter_item in iter_measure:
+			if(note_index > MAX_NUM_NOTES):
+				print("WARNING!!!  note_index is bigger than MAX_NUM_NOTES:", MAX_NUM_NOTES)
+				continue
+
 			if type(iter_item) not in [note.Note, note.Rest, chord.Chord]:
 				continue
-			else:
-				nth_index += 1
 
-			iter_vector = vector[:][nth_index]
-			# vector[0][nth_index][0] = 1  # Show that it is not rest
+
+			iter_onehot_vector = np.zeros(onehot_size)
+
+
 			iter_note = None
+
 			if type(iter_item) is note.Note:
-				iter_note = iter_item
+				iter_note = iter_item.transpose(steps_to_move) # make note in diatonic scale
 			elif type(iter_item) is chord.Chord:
-				iter_note = iter_item[-1]   # get the hightst picth in the chord for now. We will get all notes in chord in the future
+				iter_note = iter_item[-1].transpose(steps_to_move)   # get the highest picth in the chord for now. We will get all notes in chord in the future
 			elif type(iter_item) is note.Rest:
-				iter_vector[0] = 1  # Might vector[nth_index][0] = 1
+				iter_onehot_vector[0] = 1
 				iter_duration = iter_item.duration.quarterLength
 				duration_index = transform_duration_to_number(iter_duration)
-				iter_vector[duration_pos_in_vector + duration_index] = 1
-				iter_vector[measure_at_pos_in_vector] = measure_index
-				iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
-				measure_accum_duration += iter_duration
-				continue
-			else:
+				iter_onehot_vector[duration_pos_in_vector + duration_index] = 1
+				note_index += 1
 				continue
 
 			iter_name = iter_note.name
@@ -82,39 +101,58 @@ def vectorize(part):
 			iter_octave = get_octave(iter_octave)
 			duration_index = transform_duration_to_number(iter_duration)
 
-			if(iter_name_number < 0):
+			if(iter_name_number < 0):  # Case of Cb
 				if(iter_octave != 0):
 					iter_octave -= 1
 					iter_name_number = 12 + iter_name_number
 				else:
 					print("Invalid Note. Consider it as a Rest")
-					iter_vector[0] = 1  
-					iter_vector[duration_pos_in_vector + duration_index] = 1
-					iter_vector[measure_at_pos_in_vector] = measure_index
-					iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
-					measure_accum_duration += iter_duration
+					iter_onehot_vector[0] = 1  
+					iter_onehot_vector[duration_pos_in_vector + duration_index] = 1
 					continue
 
 			if(iter_octave > 9):
 				print("Octave is too high. Consider it as a Rest")
-				iter_vector[0] = 1  
-				iter_vector[duration_pos_in_vector + duration_index] = 1
-				iter_vector[measure_at_pos_in_vector] = measure_index
-				iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
-				measure_accum_duration += iter_duration
+				iter_onehot_vector[0] = 1  
+				iter_onehot_vector[duration_pos_in_vector + duration_index] = 1
 				continue
 
-			iter_vector[octave_pos_in_vector+iter_octave] = 1
-			iter_vector[name_pos_in_vector+iter_name_number] = 1
-			iter_vector[duration_pos_in_vector+duration_index] = 1
-			iter_vector[measure_at_pos_in_vector] = measure_index
-			iter_vector[measure_left_pos_in_vector] = total_num_measures - measure_index - 1
+			iter_onehot_vector[octave_pos_in_vector+iter_octave] = 1
+			iter_onehot_vector[name_pos_in_vector+iter_name_number] = 1
+			iter_onehot_vector[duration_pos_in_vector+duration_index] = 1
+			measure_vector[note_index] = iter_onehot_vector
+			note_index += 1
 
 
-			measure_accum_duration += iter_duration
 
-		total_accum_duration += measure_accum_duration
-	return vector
+		if(measure_index < sample_measure_size - 1):  # Normal Case
+			sample_vector[0,measure_index] = measure_vector
+
+
+		else:  # Last case
+			sample_vector[0, -1] = measure_vector
+			# need to add sample_vector to ret_vector after flatten
+			flatten_sample_vector = flatten_measures_vector(sample_vector)
+			ret_vector = np.append(ret_vector, flatten_sample_vector, axis=0)
+
+
+			temp_sample_vector = np.zeros([1, sample_measure_size, MAX_NUM_NOTES, onehot_size])
+			temp_sample_vector[0, :-1, : , : ] = copy.deepcopy(sample_vector[0, 1:, :, :])
+			sample_vector = temp_sample_vector
+		
+
+
+	return ret_vector
+
+
+
+
+def flatten_measures_vector(sample_vector):
+	#the sample_vector.shape should be [1, sample_measure_size, 16, onehot_size]
+	one_, sample_measure_size, max_num_note_in_measure, onehot_size_ = sample_vector.shape
+
+	return sample_vector.reshape([one_, sample_measure_size * max_num_note_in_measure, onehot_size_])
+
 
 
 
@@ -253,7 +291,6 @@ Duration
 
 
 def vector_to_note(vector):	
-
 	choosen_notes = []
 	vector = vector.reshape(len(vector[0,:]), len(vector[0,0,:]))
 	length = vector.shape[0]
@@ -264,21 +301,43 @@ def vector_to_note(vector):
 		duration = iter_vector[22:38]
 		is_rest_name = np.append(np.array(iter_vector[0]), iter_vector[10:22])
 		if(is_rest_name.argmax() == 0):
-			choosen_duration = possible_duration[duration.argmax()]
+			max_val = max(duration)
+			if(max_val == 0):
+				choosen_duration = 0.5
+			else:
+				choosen_duration = possible_duration[duration.argmax()]
 			choosen_note = note.Rest(quarterLength = choosen_duration)
 		else:
-			choosen_octave = str(octave.argmax())
-			choosen_name = name_dic[name.argmax()]
-			choosen_duration = possible_duration[duration.argmax()]
-			choosen_note = note.Note(choosen_name+choosen_octave, quarterLength = choosen_duration)
+			# Probability octave
+			sum_ = octave.sum()
+			octave = octave / sum_
+			choosen_value = np.random.choice(octave, p = octave)
+			choosen_octave = str(np.where(octave == choosen_value)[0][0])
+			# choosen_octave = str(octave.argmax())
+			# Probability name
+			sum_ = name.sum()
+			name = name / sum_
+			choosen_name_value = np.random.choice(name, p = name)
+			choosen_name_index = np.where(name == choosen_name_value)[0][0]
+			choosen_name = name_dic[choosen_name_index]
+			# choosen_name = name_dic[name.argmax()]
+
+			# Probability duration
+			sum_ = duration.sum()
+			duration = duration / sum_
+			choosen_duration_value = np.random.choice(duration, p = duration)
+			choosen_duration_index = np.where(duration == choosen_duration_value)[0][0]
+			choosen_duration = possible_duration[choosen_duration_index]
+			# choosen_duration = possible_duration[duration.argmax()]
+			choosen_note = note.Note(choosen_name + choosen_octave, quarterLength = choosen_duration)
 		choosen_notes.append(choosen_note)
 	return choosen_notes
 
 
 
 def output_to_one_hot(vector):
-	# vecctor shape should be (40, )
-	one_hot = np.zeros(40)
+	# vecctor shape should be (38, )
+	one_hot = np.zeros(onehot_size)
 	octave = vector[1:10]
 	name = vector[10:22]
 	duration = vector[22:38]
@@ -286,7 +345,7 @@ def output_to_one_hot(vector):
 	if(is_rest_name.argmax() == 0):
 		one_hot[0] = 1
 		best_index = duration.argmax()
-		one_hot[22 + best_index] = 1;
+		one_hot[22 + best_index] = 1
 	else:
 		one_hot[1 + octave.argmax()] = 1
 		one_hot[10 + name.argmax()] = 1
@@ -298,9 +357,15 @@ def output_to_one_hot(vector):
 
 def make_output(vector):
 	# vector should shape in (sequence length, one_hot_vector length)
-	output = copy.deepcopy(vector)
-	output = output[1:, :]
+	output = np.zeros(vector.shape)
+
+	
+	# output[:,:-1,:] = copy.deepcopy(vector[:,1:,:])   # For measure slide
+
+	output[:-1,:,:] = vector[1:,:,:]  # For notes slide
+
 	return output
+
 
 
 def pad_sequences(vector, maxlen=None):
@@ -313,7 +378,7 @@ def pad_sequences(vector, maxlen=None):
 
 
 
-def mxl_to_vector(mxl_file, maxlen=500, clef="treble"):
+def mxl_to_vector(mxl_file, measure_size=2, bundle_size=20, maxlen=600, clef="treble"):
 	summer = converter.parse(mxl_file)
 	parts = []
 	part = None	
@@ -327,23 +392,22 @@ def mxl_to_vector(mxl_file, maxlen=500, clef="treble"):
 		part = summer.getElementsByClass('Part')[0]
 
 
-	vector = vectorize(part)
+	key_ = get_key(part)
 
-	origin_length, one_hot_length = vector.shape
-
-	print(mxl_file, " length:", origin_length)
+	# vector = vectorize(part, measure_size, key_.sharps)   # Slide window by measure
+	vector = slide_window(part, bundle_size=bundle_size, scale_int=key_.sharps)  # Slide window by notes
 
 
 	output = make_output(vector)
 
-	vector = pad_sequences(vector, maxlen=maxlen)
-	output = pad_sequences(output, maxlen=maxlen)
+	# vector = pad_sequences(vector, maxlen=maxlen)
+	# output = pad_sequences(output, maxlen=maxlen)
 
-	seq_length, one_hot_length = vector.shape
+	# seq_length, one_hot_length = vector.shape
 
-	vector = vector.reshape(1, maxlen, one_hot_length)   #(number of training data, sequence length, one_hot_vector length)
+	# vector = vector.reshape(1, maxlen, one_hot_length)   #(number of training data, sequence length, one_hot_vector length)
 
-	output = output.reshape(1, maxlen, one_hot_length) 
+	# output = output.reshape(1, maxlen, one_hot_length) 
 
 	return vector, output
 
@@ -363,3 +427,170 @@ def notes_to_stream(notes):
 			s.append(note)
 	return s
 
+
+
+
+
+def print_part(part):
+	for i in range(len(part)):
+		print(i, ":", part[i])
+		try:
+			for j in part[i]:
+				print(j)
+		except:
+			continue
+		print()
+
+
+
+def get_key(part):
+	for i in part:
+		try:
+			part_key = i.getElementsByClass('KeySignature')
+			if(len(part_key) > 0):
+				key_ = part_key[0]
+				return key_
+		except:
+			continue
+
+
+
+
+
+
+
+
+def vectorize(part, scale_int=0):
+	measures = part.getElementsByClass('Measure')
+	total_num_measures = len(measures)
+	num_all_notes = count_notes(part)
+	if(num_all_notes > 600):
+		print("Number of Notes are bigger than maximum size: ", num_all_notes)
+		return None
+	vector = np.zeros([600, onehot_size]) 
+	total_accum_duration = 0
+	nth_index = -1
+	measure_index = 0
+	steps_to_move = sharps_to_diatonic_dic[scale_int]
+	for measure in measures:
+		measure_index += 1
+		measure_accum_duration = 0
+		for iter_item in measure:
+			if type(iter_item) not in [note.Note, note.Rest, chord.Chord]:
+				continue
+			else:
+				nth_index += 1
+
+			iter_vector = vector[:][nth_index]
+			# vector[0][nth_index][0] = 1  # Show that it is not rest
+			iter_note = None
+			if type(iter_item) is note.Note:
+				iter_note = iter_item.transpose(steps_to_move)
+			elif type(iter_item) is chord.Chord:
+				iter_note = iter_item[-1].transpose(steps_to_move)   # get the hightst picth in the chord for now. We will get all notes in chord in the future
+			elif type(iter_item) is note.Rest:
+				iter_vector[0] = 1  # Might vector[nth_index][0] = 1
+				iter_duration = iter_item.duration.quarterLength
+				duration_index = transform_duration_to_number(iter_duration)
+				iter_vector[duration_pos_in_vector + duration_index] = 1
+				continue
+			else:
+				continue
+
+			iter_name = iter_note.name
+			iter_octave = iter_note.octave
+			iter_duration = iter_note.duration.quarterLength
+
+			iter_name_number = transform_name_to_number(iter_name)
+			iter_octave = get_octave(iter_octave)
+			duration_index = transform_duration_to_number(iter_duration)
+
+			if(iter_name_number < 0):
+				if(iter_octave != 0):
+					iter_octave -= 1
+					iter_name_number = 12 + iter_name_number
+				else:
+					print("Invalid Note. Consider it as a Rest")
+					iter_vector[0] = 1  
+					iter_vector[duration_pos_in_vector + duration_index] = 1
+					continue
+
+			if(iter_octave > 9):
+				print("Octave is too high. Consider it as a Rest")
+				iter_vector[0] = 1  
+				iter_vector[duration_pos_in_vector + duration_index] = 1
+				continue
+
+			iter_vector[octave_pos_in_vector+iter_octave] = 1
+			iter_vector[name_pos_in_vector+iter_name_number] = 1
+			iter_vector[duration_pos_in_vector+duration_index] = 1
+
+
+	return vector
+
+
+
+def slide_window(part, bundle_size=10, scale_int=0):
+
+	vector = vectorize(part, scale_int=scale_int)
+
+	sample_size = len(vector) - bundle_size + 1
+	ret_vector = np.empty(shape=(sample_size, bundle_size, onehot_size))
+
+	for i in range(sample_size):
+		bundle_vector = vector[i:i+bundle_size, :]
+		ret_vector[i] = bundle_vector
+
+	return ret_vector
+
+
+
+def note_to_vector(note):
+	onehot_vector = np.zeros(onehot_size)
+	name_number = transform_name_to_number(note.name)
+	octave = get_octave(note.octave)
+	duration_index = transform_duration_to_number(note.duration.quarterLength)
+	if(name_number < 0):  # Case of Cb
+		if(octave != 0):
+			octave -= 1
+			name_number = 12 + name_number
+		else:
+			print("Invalid Note. Consider it as a Rest")
+			onehot_vector[0] = 1  
+			onehot_vector[duration_pos_in_vector + duration_index] = 1
+	elif(octave > 9):
+		print("Octave is too high. Consider it as a Rest")
+		onehot_vector[0] = 1  
+		onehot_vector[duration_pos_in_vector + duration_index] = 1
+	else:
+		onehot_vector[octave_pos_in_vector + octave] = 1
+		onehot_vector[name_pos_in_vector + name_number] = 1
+		onehot_vector[duration_pos_in_vector + duration_index] = 1
+	return onehot_vector
+
+
+
+
+
+def generate_music(model, bundle_size=10):
+	first_note_name =  name_dic[np.random.choice(len(name_dic))]
+	first_note_duration = np.random.choice([0.25, 0.5, 1.0, 2.0, 4.0])
+	first_note_octave = np.random.choice(['4', '5'])
+	first_note = note.Note(first_note_name + first_note_octave, quarterLength = first_note_duration)
+	predict_one_hots = note_to_vector(first_note).reshape(1, 1, onehot_size)
+	predict_notes = [first_note]
+	for i in range(1, 400):
+		print(i, "/", 400)
+		outcome = model.predict(predict_one_hots)
+		latest_outcomt = outcome[0, -1]
+		predict_note = vector_to_note(latest_outcomt.reshape(1, 1, onehot_size))
+		predict_notes.extend(predict_note)
+		predict_one_hot = output_to_one_hot(latest_outcomt)
+		if(len(predict_one_hots[0]) < 10):
+			predict_one_hots = np.append(predict_one_hots[0], predict_one_hot.reshape(1, onehot_size), axis=0)
+			len_so_far, no = predict_one_hots.shape
+			predict_one_hots = predict_one_hots.reshape(1, len_so_far, onehot_size)
+		else:
+			predict_one_hots[0, :-1, :] = predict_one_hots[0, 1:, :]
+			predict_one_hots[0, -1] = predict_one_hot
+	return predict_notes
--- train_with_keras.py
+++ train_with_keras.py
@@ -1,5 +1,5 @@
 from keras.layers import Dense, LSTM, Dropout, Activation, Input
-from keras.models import Model, Sequential
+from keras.models import Model, Sequential, load_model
 from keras.optimizers import RMSprop
 import numpy as np
 import Vecterize as vec
@@ -9,8 +9,8 @@ from fractions import Fraction
 import copy
 
 maxlen = 1000
-one_hot_length = 40
-
+bundle_size = 30
+onehot_size = vec.onehot_size
 summer = "Summer_Joe_Hisaishi.mxl"
 sparkle = "Sparkle.mxl"
 first_love = "First_Love.mxl"
@@ -18,81 +18,99 @@ one_summers_day = "One_Summers_Day.mxl"
 river_flows_in_you = "River_Flows_In_You.mxl"
 
 
-
-summer_treble_input, summer_treble_output = vec.mxl_to_vector(summer, maxlen=maxlen, clef="treble")
-summer_bass_input, summer_bass_output = vec.mxl_to_vector(summer, maxlen=maxlen, clef="bass")
-
-first_love_treble_input, first_love_treble_output = vec.mxl_to_vector(first_love, maxlen=maxlen, clef="treble")
-first_love_bass_input, first_love_bass_output = vec.mxl_to_vector(first_love, maxlen=maxlen, clef="bass")
-
-one_summers_day_treble_input, one_summers_day_treble_output = vec.mxl_to_vector(one_summers_day, maxlen=maxlen, clef="treble")
-one_summers_day_bass_input, one_summers_day_bass_output = vec.mxl_to_vector(one_summers_day, maxlen=maxlen, clef="bass")
-
-river_flows_in_you_treble_input, river_flows_in_you_treble_output = vec.mxl_to_vector(river_flows_in_you, maxlen=maxlen, clef="treble")
-river_flows_in_you_bass_input, river_flows_in_you_bass_output = vec.mxl_to_vector(river_flows_in_you, maxlen=maxlen, clef="bass")
-
-sparkle_treble_input, sparkle_treble_output = vec.mxl_to_vector(sparkle, maxlen=maxlen, clef="treble")
-sparkle_bass_input, sparkle_bass_output = vec.mxl_to_vector(sparkle, maxlen=maxlen, clef="bass")
+# summer = converter.parse(first_love)
+# part = summer.getElementsByClass('Part')[0]
+# key_ = vec.get_key(part)
+# vector = vec.vectorize(part, 2, key_.sharps)
 
 
 
+summer_treble_input, summer_treble_output = vec.mxl_to_vector(summer, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="treble")
+summer_bass_input, summer_bass_output = vec.mxl_to_vector(summer, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="bass")
+
+first_love_treble_input, first_love_treble_output = vec.mxl_to_vector(first_love, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="treble")
+first_love_bass_input, first_love_bass_output = vec.mxl_to_vector(first_love, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="bass")
+
+one_summers_day_treble_input, one_summers_day_treble_output = vec.mxl_to_vector(one_summers_day, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="treble")
+one_summers_day_bass_input, one_summers_day_bass_output = vec.mxl_to_vector(one_summers_day, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="bass")
+
+river_flows_in_you_treble_input, river_flows_in_you_treble_output = vec.mxl_to_vector(river_flows_in_you, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="treble")
+river_flows_in_you_bass_input, river_flows_in_you_bass_output = vec.mxl_to_vector(river_flows_in_you, measure_size=1, bundle_size=bundle_size, maxlen=maxlen, clef="bass")
+
+# sparkle_treble_input, sparkle_treble_output = vec.mxl_to_vector(sparkle, measure_size=1, maxlen=maxlen, clef="treble")
+# sparkle_bass_input, sparkle_bass_output = vec.mxl_to_vector(sparkle, measure_size=1, maxlen=maxlen, clef="bass")
 
 
-input_ = np.empty([5, maxlen, one_hot_length])
-output_ = np.empty([5, maxlen, one_hot_length])
+input_ = np.empty(shape=[0, bundle_size, vec.onehot_size])
+
+input_ = np.append(input_, summer_treble_input, axis=0)
+input_ = np.append(input_, first_love_treble_input, axis=0)
+input_ = np.append(input_, one_summers_day_treble_input, axis=0)
+input_ = np.append(input_, river_flows_in_you_treble_input, axis=0)
+
+input_ = np.append(input_, summer_bass_input, axis=0)
+input_ = np.append(input_, first_love_bass_input, axis=0)
+input_ = np.append(input_, one_summers_day_bass_input, axis=0)
+input_ = np.append(input_, river_flows_in_you_bass_input, axis=0)
 
 
-input_[0] = summer_treble_input[0]
-input_[1] = first_love_treble_input[0]
-input_[2] = one_summers_day_treble_input[0]
-input_[3] = river_flows_in_you_treble_input[0]
-input_[4] = sparkle_treble_input[0]
 
-output_[0] = summer_treble_output[0]
-output_[1] = first_love_treble_output[0]
-output_[2] = one_summers_day_treble_output[0]
-output_[3] = river_flows_in_you_treble_output[0]
-output_[4] = sparkle_treble_output[0]
+output_ = np.empty(shape=[0, bundle_size, vec.onehot_size])
 
+output_ = np.append(output_, summer_treble_output, axis=0)
+output_ = np.append(output_, first_love_treble_output, axis=0)
+output_ = np.append(output_, one_summers_day_treble_output, axis=0)
+output_ = np.append(output_, river_flows_in_you_treble_output, axis=0)
+
+output_ = np.append(output_, summer_bass_output, axis=0)
+output_ = np.append(output_, first_love_bass_output, axis=0)
+output_ = np.append(output_, one_summers_day_bass_output, axis=0)
+output_ = np.append(output_, river_flows_in_you_bass_output, axis=0)
+
+
+# input_ = np.empty([10, maxlen, onehot_size])
+# output_ = np.empty([10, maxlen, onehot_size])
+
+# input_[0] = summer_treble_input[0]
+# input_[1] = first_love_treble_input[0]
+# input_[2] = one_summers_day_treble_input[0]
+# input_[3] = river_flows_in_you_treble_input[0]
+# input_[4] = sparkle_treble_input[0]
+# input_[5] = summer_bass_input[0]
+# input_[6] = first_love_bass_input[0]
+# input_[7] = one_summers_day_bass_input[0]
+# input_[8] = river_flows_in_you_bass_input[0]
+# input_[9] = sparkle_bass_input[0]
+
+# output_[0] = summer_treble_output[0]
+# output_[1] = first_love_treble_output[0]
+# output_[2] = one_summers_day_treble_output[0]
+# output_[3] = river_flows_in_you_treble_output[0]
+# output_[4] = sparkle_treble_output[0]
+# output_[5] = summer_bass_output[0]
+# output_[6] = first_love_bass_output[0]
+# output_[7] = one_summers_day_bass_output[0]
+# output_[8] = river_flows_in_you_bass_output[0]
+# output_[9] = sparkle_bass_output[0]
 
 
 model = Sequential()
-model.add(LSTM(256, input_shape=(None, one_hot_length), return_sequences=True))
+model.add(LSTM(256, input_shape=(None, onehot_size), return_sequences=True))
 model.add(Dropout(0.3))
 model.add(LSTM(512, return_sequences=True))
 model.add(Dropout(0.3))
 model.add(LSTM(256, return_sequences=True))
 model.add(Dense(256))
 model.add(Dropout(0.3))
-model.add(Dense(one_hot_length))
+model.add(Dense(onehot_size))
 model.add(Activation('softmax'))
-optimizer = RMSprop(lr=0.005, rho=0.9, epsilon=None, decay=1e-5)
-model.compile(loss = 'categorical_crossentropy', optimizer=optimizer)
-model.fit(input_, output_, nb_epoch=10000, batch_size=2, verbose=2)
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+model.fit(input_, output_, nb_epoch=10000, batch_size=10, verbose=2)
 
 
 
-first_note = first_love_treble_input[0, 5]
-first_note = first_note.reshape(1, 1, one_hot_length)
-outcome = model.predict(first_note)
-predict_notes = vec.vector_to_note(outcome)
-predict_one_hots = vec.output_to_one_hot(outcome.reshape([one_hot_length])).reshape([1,1,one_hot_length])
-
-
-
-for i in range(1, 400):
-	print(i, "/", 400)
-	outcome = model.predict(predict_one_hots)
-	predict_note = vec.vector_to_note(outcome[0, -1].reshape(1,1,one_hot_length))
-	predict_notes.extend(predict_note)
-	latest_outcomt = outcome[0, -1]
-	predict_one_hot = vec.output_to_one_hot(latest_outcomt)
-	predict_one_hots = np.append(predict_one_hots[0], predict_one_hot.reshape(1, one_hot_length), axis=0)
-	len_so_far, no = predict_one_hots.shape
-	predict_one_hots = predict_one_hots.reshape(1, len_so_far, one_hot_length)
-
-
-
+predict_notes = vec.generate_music(model, bundle_size=bundle_size)
 s = vec.notes_to_stream(predict_notes)
 s.show()
 
@@ -100,15 +118,19 @@ s.show()
 
 
 
+
+
+
+
 # This is  another way from https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html
-# encoder = LSTM(one_hot_length, input_shape=(seq_length,one_hot_length), return_state=True)  # I guess seq_length suppose to be None??
+# encoder = LSTM(onehot_size, input_shape=(seq_length,onehot_size), return_state=True)  # I guess seq_length suppose to be None??
 # encoder_inputs = tf.convert_to_tensor(vector, dtype='float32')
 # encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 # encoder_states = [state_h, state_c]
 
 
 # decoder_inputs = Input(shape=(None, seq_length))
-# decoder_lstm = LSTM(one_hot_length, return_sequences=True, return_state=True)
+# decoder_lstm = LSTM(onehot_size, return_sequences=True, return_state=True)
 # decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
 # decoder_dense = Dense(seq_length, activation='softmax')
 
